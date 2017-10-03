4a740db7e2d43ee8542d1f895240bb16498b8a00
Brian Roldan <brian.roldan15@gmail.com>
2017-10-03 22:58:56 +1300

    Added validating of list spot arguments

--- src/main/java/io/hatdog/ysf/service/SpotServiceImplementation.java
+++ src/main/java/io/hatdog/ysf/service/SpotServiceImplementation.java
@@ -5,19 +5,24 @@ import org.springframework.stereotype.Service;
 import io.hatdog.ysf.controller.argument.ListSpotArgument;
 import io.hatdog.ysf.controller.command.ListSpotCommand;
 import io.hatdog.ysf.repository.SpotRepository;
+import io.hatdog.ysf.validator.ListSpotArgumentValidator;
 
 @Service
 public class SpotServiceImplementation implements SpotService {
 
-	private SpotRepository spotRepository;
+	private final SpotRepository spotRepository;
+	private final ListSpotArgumentValidator listSpotArgumentValidator;
 	
-	public SpotServiceImplementation(SpotRepository spotRepository) {
+	public SpotServiceImplementation(SpotRepository spotRepository, ListSpotArgumentValidator listSpotArgumentValidator) {
 		super();
 		this.spotRepository = spotRepository;
+		this.listSpotArgumentValidator = listSpotArgumentValidator;
 	}
 
 	@Override
 	public ListSpotCommand getSpots(ListSpotArgument argument) {
+		listSpotArgumentValidator.validate(argument);		
+		
 		ListSpotCommand listSpotCommand = new ListSpotCommand();
 		
 		listSpotCommand.setSpots(spotRepository.getNearbySpots(argument));
--- src/test/java/io/hatdog/ysf/service/SpotServiceImplementationTest.java
+++ src/test/java/io/hatdog/ysf/service/SpotServiceImplementationTest.java
@@ -1,6 +1,7 @@
 package io.hatdog.ysf.service;
 
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.MockitoAnnotations.initMocks;
@@ -15,12 +16,14 @@ import io.hatdog.ysf.controller.argument.ListSpotArgument;
 import io.hatdog.ysf.controller.command.ListSpotCommand;
 import io.hatdog.ysf.repository.SpotRepository;
 import io.hatdog.ysf.service.SpotServiceImplementation;
+import io.hatdog.ysf.validator.ListSpotArgumentValidator;
 
 public class SpotServiceImplementationTest {
 
 	SpotServiceImplementation spotService;
 	
 	@Mock SpotRepository spotRepository;
+	@Mock ListSpotArgumentValidator listSpotArgumentValidator;
 
 	final BigDecimal TEST_LATITUDE = new BigDecimal(14.553016);
 	final BigDecimal TEST_LONGITUDE = new BigDecimal(121.049116);
@@ -28,18 +31,18 @@ public class SpotServiceImplementationTest {
 	@Before
 	public void setUp(){
 		initMocks(this);
-		spotService = new SpotServiceImplementation(spotRepository);
+		spotService = new SpotServiceImplementation(spotRepository, listSpotArgumentValidator);
 	}
 	
 	@Test
 	public void basicPositiveTest(){
-		
 		ListSpotArgument argument = ListSpotArgument.builder().latitude(TEST_LATITUDE).longitude(TEST_LONGITUDE).build();
 		
 		ListSpotCommand listSpotCommand = spotService.getSpots(argument);
 		
 		assertNotNull(listSpotCommand);
-		verify(spotRepository, times(1)).getNearbySpots(argument);
+		verify(listSpotArgumentValidator, times(1)).validate(eq(argument));
+		verify(spotRepository, times(1)).getNearbySpots(eq(argument));
 	}
 	
 }
